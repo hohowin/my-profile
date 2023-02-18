fe4f81dd3ccdc2f7e41873fb3729d91d6bb3d8fe
Howin Ho <ho1ho@yahoo.com>
2023-02-17 20:54:02 -0500

    added aos effect

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # my-profile
+
+## Setup
+
+```bash
+ng new my-profile
+yarn add -D @types/aos
+yarn add aos
+ng add @angular/material
+```
+
+In `angular.json`, add:
+
+```
+“node_modules/aos/dist/aos.css”
+```
+
+Checkout: https://github.com/michalsnik/aos
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
